ab8ba539bcf8233107207e65c68dd1da7cbf46c3
maria-chiara-iero <ierovante.1952556@studenti.uniroma1.it>
2023-04-25 00:04:30 +0200

    central part

--- src/components/Game/Game.vue
+++ src/components/Game/Game.vue
@@ -6,5 +6,18 @@ export default {
 </script>
 
 <template>
-  <div>Game is going here</div>
+  <div style="width: 60%;
+        position: absolute;
+        top: 0;
+        left: 20%;
+        height: 100vh;
+        padding: 20px;
+        background-color: #171616;
+        color: white;">
+         <table style= "width: 50%; height: 50%; background-color: #811394; margin-top: 12%; margin-left: 27%;">
+          <tr>
+            <td style="width: 100%; height: 0; padding-bottom: 100%;"> </td>
+          </tr>
+        </table>
+        </div>
 </template>
