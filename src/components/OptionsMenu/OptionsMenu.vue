a8fa97db5f94284a5ca4854222548501be84f930
metanfetaminkia <iannificorilli.1950634@studenti.uniroma1.it>
2023-04-25 21:16:44 +0200

    navbar con icone

--- src/components/OptionsMenu/OptionsMenu.vue
+++ src/components/OptionsMenu/OptionsMenu.vue
@@ -9,28 +9,10 @@
 </script>
 
 <template>
+
     <div class="options-menu">
-        <h1>Chopstick</h1>
-        <table>
-            <thead>
-                <tr onclick="alert('Cliccato sulla prima riga!')">
-                    <td><font size="5">Play</font></td>
-                </tr>
-                <tr onclick="alert('Cliccato sulla seconda riga!')">
-                    <td><font size="5">Recent Game</font></td>
-                </tr>
-                <tr onclick="alert('Cliccato sulla terza riga!')">
-                    <td><font size="5">Stats</font></td>
-                </tr>
-                <tr onclick="alert('Cliccato sulla quarta riga!')">
-                    <td><font size="5">Friends</font></td>
-                </tr>
-                <tr onclick="alert('Cliccato sulla quinta riga!')">
-                    <td><font size="5">Settings</font></td>
-                </tr>
-            </thead>
-        </table>
-    </div>
+       
+     </div>
        
    
 </template>
