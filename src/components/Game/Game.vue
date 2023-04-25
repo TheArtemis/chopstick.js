772d6d2e1bc6b4557ebb2bb2eecfc3ea59bebe93
TheArtemis <lordef01@gmail.com>
2023-04-25 13:56:17 +0200

    Separated the playerhand component

--- src/components/Game/Game.vue
+++ src/components/Game/Game.vue
@@ -1,7 +1,11 @@
 <script>
+import PlayerHand from './PlayerHand.vue'
+
 export default {
-    name: 'Game',  
-    
+    name: 'Game',
+    components: {
+        PlayerHand,
+    },  
 }
 </script>
 
@@ -13,23 +17,9 @@ export default {
         <div class="hand opponent left"></div>      
     </div>
     <div class="game-player">
-      <div class="hand player left"></div>
-      <div class="hand player right"></div>
+      <PlayerHand class="hand player left"/>
+      <PlayerHand class="hand player right"/>
     </div>
     </div>        
-  </div>
-  <!-- <div style="width: 60%;
-        position: absolute;
-        top: 0;
-        left: 20%;
-        height: 100vh;
-        padding: 20px;
-        background-color: #171616;
-        color: white;">
-         <table style= "width: 50%; height: 50%; background-color: #811394; margin-top: 12%; margin-left: 27%;">
-          <tr>
-            <td style="width: 100%; height: 0; padding-bottom: 100%;"> </td>
-          </tr>
-        </table>
-        </div> -->
+  </div>  
 </template>
---
+++ src/components/Game/PlayerHand.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        name: 'PlayerHand',
+    }
+</script>
+
+<template>
+    <div                
+      @mousedown="startDrag"
+      @touchstart="startDrag"
+      @mouseup="stopDrag"
+      @touchend="stopDrag"
+      ></div>
+</template>
