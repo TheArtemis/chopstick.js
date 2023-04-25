c170a3f640901bb18a4784bd1017878d0dccf159
metanfetaminkia <iannificorilli.1950634@studenti.uniroma1.it>
2023-04-25 21:17:52 +0200

    navbar con icone

--- src/components/OptionsMenu/OptionsMenu.vue
+++ src/components/OptionsMenu/OptionsMenu.vue
@@ -9,10 +9,17 @@
 </script>
 
 <template>
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <div class="options-menu">
-       
+        <div class="sidebar">
+            <h1>Chopsticks</h1>
+                <ul>
+                    <li><i class="fa-solid fa-user"></i><font size="5">   Profile</font></li>
+                    <li><i class="fa-solid fa-list-ul"></i><font size="5">   Recent Games</font></li>
+                    <li><i class="fa-solid fa-chart-simple"></i><font size="5">   Stats</font></li>
+                    <li><i class="fa-solid fa-users"></i><font size="5">   Friends</font></li>
+                    <li><i class="fa-solid fa-gear"></i><font size="5">   Settings</font></li>
+               </ul>
+        </div>
      </div>
-       
-   
 </template>
