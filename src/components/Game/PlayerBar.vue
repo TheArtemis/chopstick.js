e08a705c16ae5c019e6c0e830713ba85322d8d95
TheArtemis <lordef01@gmail.com>
2023-04-30 14:44:53 +0200

    added playerbar

---
+++ src/components/Game/PlayerBar.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        name: 'PlayerBar',
+        props: {
+            player : Object,
+        }
+    };
+</script>
+
+<template>
+    <div class = "player-bar">
+        <div class = "player-bar-picture">
+            <img :src = "player.picture" alt = "player picture">
+        </div>
+        <div class = "player-bar-name">
+            <h3>{{player.name}}</h3>
+        </div>
+    </div>
+</template>
