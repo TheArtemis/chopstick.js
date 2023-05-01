6c132685a2c737aa57823fd7d59fda34e3fca5b2
metanfetaminkia <iannificorilli.1950634@studenti.uniroma1.it>
2023-05-01 18:09:44 +0200

    profile adjusted

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,10 +1,15 @@
 <template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
   <div class="contenitor">
      <div class="profile">
         <h2>Profile</h2>
+        <div class="username">
+            <label> Username</label>
+        </div>
         <form>
-
-            <div class="inputProfile">
+           
+            <!-- <div class="inputProfile">
                 <input type="text" name="name" required value="">
                 <label>Name</label>
             </div>
@@ -22,12 +27,18 @@
             <div class="inputProfile">
                 <input type="password" name="password" required value="">
                 <label>Password</label>
-            </div>
+            </div> -->
             <div class="picture">
-                <div class="upload-btn">
-                  <input type="file" name="img_btn" accept="image/*">
+                <div class="pfp_label">
+                    <label>Profile Picture</label>
                 </div>
-                <div class="img"></div>
+                <!-- <div class="upload-btn">
+                  <input type="file" name="img_btn" accept="image/*">
+                </div> -->
+                <div class="img">
+                    <input type="file" id="img_btn" accept="image/*">
+                    <label for="img_btn" class="img-upload" size="60">Niente</label>
+                </div>
                 <textarea class="bio">
                  </textarea>
                 <label>Biography</label>
@@ -37,6 +48,31 @@
         </form>
      </div>
     </div>
+    <div class="right-container">
+		<div class="top-row">
+            <h2>Statistics</h2>
+              <div class="top-wrapper">
+                <div class ="top-container">
+                    <i class="fa-regular fa-thumbs-up"></i>
+                     <span class="wins" data-val="400">000</span>
+                     <span class="wins-text">Wins</span>
+                </div>
+                <div class ="top-container">
+                    <i class="fa-regular fa-thumbs-down"></i>
+                     <span class="wins" data-val="400">000</span>
+                     <span class="wins-text">Losses</span>
+                </div>
+                <div class ="top-container">
+                    <i class="fa-regular fa-hand-peace"></i>
+                     <span class="wins" data-val="400">000</span>
+                     <span class="wins-text">Ties</span>
+                </div>
+              </div>
+        </div>
+		<div class="bottom-row">
+            <h2>Recent Games</h2>
+        </div>
+	</div>
 </template>
 
 <script>
