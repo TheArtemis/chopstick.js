39aa0f40a6fa90f21a48af276078d4ab40066f46
TheArtemis <lordef01@gmail.com>
2023-05-02 20:20:10 +0200

    Fixed navbar

--- src/components/OptionsMenu/OptionsMenu.vue
+++ src/components/OptionsMenu/OptionsMenu.vue
@@ -4,7 +4,7 @@
         props:{
             navbarActive: {
                 type: Boolean,
-                default: false,
+                default: true,
             },        
         },        
     }
