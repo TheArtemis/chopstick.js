5a81344f6c4886f3296bba8d3f98169d9498ee05
TheArtemis <lordef01@gmail.com>
2023-05-03 18:16:16 +0200

    Computer beating you bug fixed

--- src/pages/Computer.vue
+++ src/pages/Computer.vue
@@ -16,6 +16,7 @@
       return {
         hasGameStarted: false,
         navbarActive: true,
+        buttonSwitch: true, /* When true the NEW GAME button is on and the SURRENDER is of. When it's false is vice-versa */
       }
     },
     methods: {
@@ -37,6 +38,10 @@
         /* console.log("navbar disabled on computer")
         console.log(this.navbarActive) */
       },
+      endGame() {
+        console.log("end game catched");
+        this.hasGameStarted = !this.hasGameStarted;
+      },      
     },    
   }
 
@@ -49,10 +54,12 @@
     <Game      
       :hasGameStarted="this.hasGameStarted"
       @enable-navbar="this.enableNavbar"
-      @disable-navbar="this.disableNavbar"/>    
+      @disable-navbar="this.disableNavbar"
+      @game-ended="this.endGame"/>    
     <PlayComputerMenu
       @start-game="this.startGame"
       @surrender-game="this.surrenderGame"
-      :hasGameStarted="this.hasGameStarted"/>
+      :hasGameStarted="this.hasGameStarted"
+      />
   </div>  
 </template>
