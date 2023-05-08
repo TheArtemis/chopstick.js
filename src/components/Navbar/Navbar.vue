7c99387311ef7bf165feab9f182c1ea8406551bf
maria-chiara-iero <ierovante.1952556@studenti.uniroma1.it>
2023-05-09 00:15:20 +0200

    nav stats

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -51,7 +51,7 @@ export default {
             </li>
 
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="/stats" class="nav-link">
                     <svg xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                         <path
