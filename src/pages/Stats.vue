d7f16e292ba8a0539a78cf2bf0f363ac49ceb4b1
maria-chiara-iero <ierovante.1952556@studenti.uniroma1.it>
2023-05-09 00:15:02 +0200

    stats

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,15 +1,13 @@
-<template>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap">
-    <div class="back">
-        <div class="graph"></div>
-    </div>
-
-</template>
-
 <script>
+    import Navbar from '@/components/Navbar/Navbar.vue'
+    import { Chart } from 'chart.js/auto';
+    
     export default {
         name: 'Stats',
-        created() {
+        components: {
+        Navbar,
+    },
+    created() {
     const colors = localStorage.getItem('colors');
     if (colors === 'true') {
       document.documentElement.setAttribute('data-colors', 'light');
@@ -17,5 +15,51 @@
       document.documentElement.removeAttribute('data-colors');
     }
    },
-    }
+
+    mounted() {
+        this.createLineChart();
+        this.createPolarAreaChart();
+    },
+    methods: {
+    createLineChart() {
+      const data = {
+        labels: ['1', '2', '3', '4', '5'],
+        datasets: [
+          {
+            label: '1 = win; -1 = losses; 0 = ties',
+            data: [0, -1, 1, 1, 0],
+            borderColor: '#f1cc53',
+          },
+        ],
+      };
+      const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+      };
+
+      this.$refs.lineChart = new Chart(this.$refs.lineChart, {
+        type: 'line',
+        data,
+        options,
+      });
+    },
+  },
+}
 </script>
+
+<template>
+    <div class="wrapper">
+        <Navbar />
+        <div class="total">
+            <div class="graph">
+                <label id="Stats">Stats</label>
+                <div class="line-chart">
+                    <canvas ref="lineChart"></canvas>
+                </div>
+            </div>
+            <div class="under">
+            </div>
+        </div>
+    </div>
+</template>
+
