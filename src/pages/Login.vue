442233e90524acf50e0477e9aa9f5668dd92a43d
maria-chiara-iero <ierovante.1952556@studenti.uniroma1.it>
2023-05-08 11:22:04 +0200

    login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,25 +1,25 @@
-<template>
-    <div class="box">
-        <h2>Login</h2>
-        <form>
-            <div class="inputBox">
-                <input type="text" name="name" required value="">
-                <label>Username</label>
-            </div>
-
-            <div class="inputBox">
-                <input type="password" name="password" required value="">
-                <label>Password</label>
-            </div>
-
-            <input type="submit" name="sign-in" value="Sign In">
-            <a href="/register">Don't have an account? Register here</a>
-        </form>
-    </div>
-</template>
-
 <script>
     export default {
         name: 'Login',
     }
 </script>
+<template>
+<div class="container">
+    <div class="box">
+        <h2>Login</h2>
+        <div class="inputBox">
+            <input type="text" name="name" required value="">
+                <label>Username</label>
+        </div>
+        <div class="inputBox">
+                <input type="password" name="password" required value="">
+                <label>Password</label>
+            </div>
+        <div class="boxbottom">
+            <input type="submit" name="sign-in" value="Sign In">
+            <a href="/register">Don't have an account? Register here</a>
+        </div>
+      
+    </div>
+</div>
+</template>
