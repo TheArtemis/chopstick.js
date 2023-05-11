60a57b94765830a9a111ace593bda6db4e2da379
maria-chiara-iero <ierovante.1952556@studenti.uniroma1.it>
2023-05-12 01:53:45 +0200

    login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,31 +1,7 @@
-<template>
-	<div>
-		<div class="container">
-			<!-- <p v-if="error" class="error" :class="{ 'error-show': showError }">{{ error }}</p> -->
-			<ErrorBox :error="this.error" :showError="this.showError"
-				@close-error="() => { this.showError = false, this.error = null }" />
-			<div class="box">
-				<h2>Login</h2>
-				<form @submit.prevent="submitLogin">
-					<div class="inputBox">
-						<input required="" type="text" placeholder="Username" v-model="username">
-					</div>
-					<div class="inputBox">
-						<input required="" type="password" placeholder="Password" v-model="password">
-					</div>
-					<div class="boxbottom">
-						<button type="submit" name="sign-in">Sign In</button>
-						<router-link :to="{ path: '/register' }">Don't have an account? Register here</router-link>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</template>
-
 <script>
 import axios from 'axios';
 import ErrorBox from '@/components/ErrorBox.vue';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const API_URL = 'http://localhost:3000';
 const axiosInstance = axios.create({
@@ -98,4 +74,41 @@ export default {
 };
 </script>
 
+<template>
+	<section class="vh-100 gradient-custom">
+	  <div class="container py-1 h-100 " >
+		<div class="row d-flex justify-content-center align-items-center h-100" >
+		  <div class="col-12 col-md-8 col-lg-8 col-xl-6" >
+			<div class="card bg-dark text" style="color: #f1cc53; border-radius: 1rem;">
+			  <div class="card-body p-5 text-center" style = "background-color: #272522;">
+  
+				<div class="mb-md-5 mt-md-4 pb-5">
+  
+				  <h2 class="fw-bold mb-2 text-uppercase" style="color: #f1cc53;">Login</h2>
+				  <p class="text-50 mb-5" style="color: #f1cc53;">Please enter your login and password!</p>
+  
+				  <div class="form-outline form mb-4" style="color: #f1cc53;">
+					<input type="email" id="typeEmailX" class="form-control form-control-lg" style="color: #f1cc53; border-color: #f1cc53; background-color: #272522;" />
+					<label class="form-label" for="typetextX" style="color: #f1cc53;">Username</label>
+				  </div>
+  
+				  <div class="form-outline form mb-4" style="color: #f1cc53;">
+					<input type="password" id="typePasswordX" class="form-control form-control-lg" style="color: #f1cc53; border-color: #f1cc53; background-color: #272522;" />
+					<label class="form-label" for="typePasswordX" style="color: #f1cc53;">Password</label>
+				  </div>
+  
+				  <button class="btn btn-outline btn-lg px-5" type="submit" style="color: #f1cc53; border-color: #f1cc53; background-color: #272522;">Login</button>
+				</div>
+				<div>
+				  <p class="mb-0" style="color: #f1cc53;">Don't have an account? <a href="/register" class="text-50 fw-bold " style="color: #f1cc53;">Sign Up</a>
+				  </p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</section>
+  </template>
 
+  
