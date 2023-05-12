24bbef9ed53d21d97c1ff3e6578e0144837dc2d0
TheArtemis <lordef01@gmail.com>
2023-05-12 19:04:12 +0200

    updated router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
-    {path: '', name: 'home', component: () => import('@/pages/Home.vue')},
+    {path: '/', redirect: '/home'},    
+    {path: '/home', name: 'home', component: () => import('@/pages/Home.vue')},
     {path: '/play', name: 'play', component: () => import('@/pages/Play.vue')},
     {path: '/play/computer', name: 'computer', component: () => import('@/pages/Computer.vue')},
     {path: '/login', name: 'login', component: () => import('@/pages/Login.vue')},
@@ -9,6 +10,7 @@ const routes = [
     {path: '/stats', name: 'stats', component: () => import('@/pages/Stats.vue')},
     {path: '/register', name: 'register', component: () => import('@/pages/Register.vue')},
     {path: '/settings', name: 'settings', component: () => import('@/pages/Settings.vue')},
+    
 ]
 
 const router = createRouter({
