3de132bc61ef55f850e25c4d49f82c309152e623
TheArtemis <lordef01@gmail.com>
2023-05-22 22:29:16 +0200

    Reversed recent Games

--- src/components/RecentGames/recentGames.vue
+++ src/components/RecentGames/recentGames.vue
@@ -2,19 +2,19 @@
 import recentGame from '@/components/RecentGames/recentGame.vue'
 
 export default {
-    name: 'recentGames',
-    created() {
+  name: 'recentGames',
+  created() {
     const colors = localStorage.getItem('colors');
     if (colors === 'true') {
       document.documentElement.setAttribute('data-colors', 'light');
     } else {
       document.documentElement.removeAttribute('data-colors');
     }
-   },
-   components: {
+  },
+  components: {
     recentGame,
   },
-  props:{
+  props: {
     recentGamesList: Array,
   },
 }
@@ -22,15 +22,9 @@ export default {
 
 <template>
   <div class="recent-games-container">
-    <div v-for="(game, index) in this.recentGamesList" :key="index">
-      <recent-game
-        :player1="game.player1"
-        :rating1="game.rating1"
-        :player2="game.player2"
-        :rating2="game.rating2"
-        :winner="game.winner"
-        :date="game.date"
-      />
+    <div v-for="(game, index) in this.recentGamesList.slice().reverse()" :key="index">
+      <recent-game :player1="game.player1" :rating1="game.rating1" :player2="game.player2" :rating2="game.rating2"
+        :winner="game.winner" :date="game.date" />
     </div>
   </div>
 </template>
